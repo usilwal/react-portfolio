a4dca421e12122b14d7f965868ecef7192311fdc
usilwal <usilwal@hotmail.com>
2021-05-04 13:53:05 -0400

    minor fixes

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
             <img src={image} alt="bgImage" className="absolute object-cover w-full h-full"/>
             <section className="relative min-h-screen pt-12 lg:pt-64 px-8">
                 <h1 className="text-6xl text-center text-gray-100 font-bold cursive leading-none lg:leading-snug home-name">WELCOME!</h1>
-                <p className="text-6xl text-center text-gray-300 font-bold italic leading-none lg:leading-snug">-utkarsh silwal</p>
+                <p className="text-6xl text-center text-gray-300 font-bold italic leading-none lg:leading-snug">to utkarsh silwal's site</p>
             </section>
         </main>
     )
