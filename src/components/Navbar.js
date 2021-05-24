9e85c8b64a34455d1737955832886e2f1ee21c6b
usilwal <usilwal@hotmail.com>
2021-05-24 19:54:15 -0400

    minor navbar style fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,19 +8,19 @@ const Navbar = () => {
         <header className="bg-gray-700">
             <div className="container mx-auto flex justify-between">
                 <nav className="flex">
-                    <NavLink to="/" exact activeClassName="text-white"
+                    <NavLink to="/" exact activeClassName="text-green-400"
                     className="inline-flex items-center py-6 px-3 mr-4 text-gray-100 hover:text-green-400 text-4xl font-bold cursive">
                         usilwal
                     </NavLink>
-                    <NavLink to="/posts" activeClassName="text-gray-100 bg-gray-700"
+                    <NavLink to="/posts" activeClassName="text-green-400"
                     className="inline-flex items-center py-3 px-3 my-6 rounded text-gray-200 hover:text-green-400">
                         Posts
                     </NavLink>
-                    <NavLink to="/projects" activeClassName="text-gray-100 bg-gray-700"
+                    <NavLink to="/projects" activeClassName="text-green-400"
                     className="inline-flex items-center py-3 px-3 my-6 rounded text-gray-200 hover:text-green-400">
                         Projects
                     </NavLink>
-                    <NavLink to="/about" activeClassName="text-gray-100 bg-gray-700"
+                    <NavLink to="/about" activeClassName="text-green-400"
                     className="inline-flex items-center py-3 px-3 my-6 rounded text-gray-200 hover:text-green-400">
                         About
                     </NavLink>
